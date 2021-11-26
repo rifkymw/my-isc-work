98b718e3b3e7e71b7d3fa3e94fc166519fceaf1d
rifkymw <rifky.wijanarko@gmail.com>
2021-11-26 18:17:38 +0000

    finishing up exercises

---
+++ python/notebooks/data_store.py
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+
+class DataStore:
+    """
+    Store of measurements and times
+    """
+
+    def __init__(self):
+        self.times = []
+        self.measurements = []
+
+    def add_measurement(self, date, value):
+        """
+        Add the given value to the data store
+
+        :param date: Date of the measurement
+        :type date: str
+
+        :param value: Value to add
+        :type value: int or float
+        """
+
+        self.measurements.append(value)
+        self.times.append(date)
+
+    def print_measurements(self):
+        """
+        print the measurements and times
+        """
+
+        for time, value in zip(self.times, self.measurements):
+            print(time, value)
+            
+    def get_max(self):
+        
+            return max(self.measurements)
+            
+    def get_min(self):
+        
+            return min(self.measurements)
+            
+    def get_mean(self):
+        
+            return sum(self.measurements)/len(self.measurements)
+        
